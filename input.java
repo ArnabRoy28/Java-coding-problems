9151710baccda0abe4a21e672f86219b1d45d1fe
Arnab <contactarnab05@gmail.com>
2025-08-12 13:26:53 +0530

    Create input.java
    Taking input in java

---
+++ input.java
@@ -0,0 +1,10 @@
+// input and display
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int age = sc.nextInt();
+        System.out.println("My age is " + age);
+    }
+}
