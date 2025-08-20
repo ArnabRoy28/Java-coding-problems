80ee8f2540a77882ee358f3940d69508256bd6d2
Arnab <contactarnab05@gmail.com>
2025-08-21 04:19:32 +0530

    Printing asterisk pattern
    *
    **
    ***
    ****
    *****

---
+++ asterisk.java
@@ -0,0 +1,27 @@
+/*
+*
+**
+***
+****
+*****
+*/
+  
+package com.mycompany.asterisk;
+
+
+//@author arnab
+public class asterisk {
+
+    public static void main(String[] args) {
+       
+        int i,j;
+      for(i=0;i<=5;i++){
+          for(j=0;j<=i;j++){
+             System.out.print("*");
+          
+        
+         }
+          System.out.println();
+    }
+  }
+}
