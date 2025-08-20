be5d18b5a1b5c284d53666a102db5baa84164ec2
Arnab <contactarnab05@gmail.com>
2025-08-21 00:28:43 +0530

    Creating Pattern using Java
    1
    12
    123
    1234
    12345

---
+++ pattern.java
@@ -0,0 +1,25 @@
+/*
+1
+12
+123
+1234
+12345
+ @author arnab
+ */
+package com.mycompany.project;
+
+public class Project {
+
+    public static void main(String[] args) {
+       
+        int i,j;
+      for(i=0;i<=5;i++){
+          for(j=1;j<=i;j++){
+             System.out.print(j);
+          
+        
+      }
+          System.out.println();
+       }
+    }
+}
